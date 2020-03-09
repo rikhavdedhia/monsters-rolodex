6977adf3904c0fc973e46b43d964f953324aea39
rikhavdedhia <rdedh1@unh.newhaven.edu>
2020-03-08 20:06:16 -0400

    Updates

---
+++ precache-manifest.a4ef51354dfb3b625f29b731387b3a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3bc80a89b645a1b4f7d5531c54c43a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7045015b65e144aaeb4f",
+    "url": "/monsters-rolodex/static/css/main.e230bd05.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7045015b65e144aaeb4f",
+    "url": "/monsters-rolodex/static/js/main.5fef4f47.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
